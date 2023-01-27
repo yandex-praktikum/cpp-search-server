cfa7c36e23c6edc6261d5345a109328d0af57db0
Zaifat <33205124+Zaifat@users.noreply.github.com>
2023-01-27 03:08:47 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,28 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include "document.h"
+#include "paginator.h"
+#include "read_input_functions.h"
+#include "string_processing.h"
+#include "search_server.h"
+#include "request_queue.h"
+ 
+using namespace std;
+ 
+int main() {
+    SearchServer search_server("and in at"s);
+    RequestQueue request_queue(search_server);
+ 
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
+ 
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty request"s);
+    }
+    request_queue.AddFindRequest("curly dog"s);
+    request_queue.AddFindRequest("big collar"s);
+    request_queue.AddFindRequest("sparrow"s);
+    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
+} 
